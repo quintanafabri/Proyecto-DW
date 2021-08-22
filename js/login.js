f9c923404e0ea15d25f69959a8f3c3ad42d73048
quintanafabri <88584654+quintanafabri@users.noreply.github.com>
2021-08-22 17:37:20 -0300

    Obligatorio 1- dificultad

--- js/login.js
+++ js/login.js
@@ -3,4 +3,47 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+
+
+
+    
+
 });
+
+
+function conectar(){
+
+    let dato = {}
+    let usuario = document.getElementById(`usuario`).value;
+    let password = document.getElementById(`password`).value;
+    
+    // trim corta los espacios en blanco
+    if (usuario.trim() === `` || password.trim() === ``){
+    
+     alert(`usuario o email vacio`);
+    
+    
+     }else{
+     dato.usuario = usuario;
+     dato.password = password;
+     dato.estado = "conectado";
+     localStorage.setItem("dato",JSON.stringify(dato));
+     location.href ="index.html";
+    
+     }
+    }
+    
+    function desconectar(){
+      localStorage.clear();
+      location.href = "login.html";
+    }
+
+
+   
+
+
+
+
+
+
+  
--- js/products.js
+++ js/products.js
@@ -1,6 +1,52 @@
+var categoriesArray = [];
+
+function showProductList(array){
+    showSpinner();
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += 
+        
+        `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+            <div class="col-3">
+            <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+        </div>
+        <div class="col">
+        
+            <div class="d-flex w-100 justify-content-between">
+                <h4 class="mb-1">`+ category.name +  `</h4>
+                <h6 style="margin:right" >$`+ category.cost + ' ' + category.currency + ` </h6>
+
+            </div>
+            
+            <p class="mb-1">` + category.description + `</p>          
+            <small style="color:blue;" >` + category.soldCount + ` Vendidos</small>
+
+                </div>
+            </div>
+        </div>
+        `
+
+
+
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+    hideSpinner();
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProductList(categoriesArray);
+        }
+    });
 });
